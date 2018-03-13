147abfbd440047b2ba7e15832308e49d4a82895f
Toan <thton@miners.utep.edu>
2018-03-13 16:01:58 -0600

    other tiny change

--- Toan/hello.py
+++ Toan/hello.py
@@ -2,7 +2,7 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-window = Gtk.Window(title="Hello World")
+window = Gtk.Window(title="Hello World3")
 window.show()
 window.connect("destroy", Gtk.main_quit)
 Gtk.main()
