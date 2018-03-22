f5f70fe0d2ecbca3823e8b98e149664a387e1d67
Aaron <a.aronsantellanes@yahoo.com>
2018-03-22 13:51:52 -0600

    changes to window sizes

--- Aaron/PDGS_MainWindowBox.py
+++ Aaron/PDGS_MainWindowBox.py
@@ -14,42 +14,42 @@ class MainWindow(Gtk.Window):
 
 	def __init__(self):
 		Gtk.Window.__init__(self, title="Protocol Dissector Generator System")
-
+		self.set_default_size(1300, 700)
 
 		# Main Box
-		box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 5)
+		box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 0)
 		self.add(box)
 
 		#LabelBox
-		labelBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 5)
+		labelBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 0)
 		labelBox.set_homogeneous(False)
 
 		#ButtonsBox
-		buttonBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 5)
+		buttonBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 0)
 		buttonBox.set_homogeneous(False)
 
-
 		#Project/Dissector Box
-		projectDissectorBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 5)
+		projectDissectorBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 0)
 		projectDissectorBox.set_homogeneous(False)
 
 		#Project Navigator Box
-		projectNavigatorBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 5)
+		projectNavigatorBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 0)
+		projectNavigatorBox.set_size_request(10, 400)
 		projectNavigatorBox.set_homogeneous(False)
 
 		#Dissector Builder Area Box
-		dissectorBuilderAreaBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 5)
+		dissectorBuilderAreaBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 0)
 		dissectorBuilderAreaBox.set_homogeneous(False)
 
 		projectDissectorBox.pack_start(projectNavigatorBox, True, True, 5)
 		projectDissectorBox.pack_start(dissectorBuilderAreaBox, True, True, 5)
 
-
 		#PacketPreviewAreaBox
-		packetPreviewAreaBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 5)
+		packetPreviewAreaBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 0)
+		packetPreviewAreaBox.set_size_request(1300, 100)
 		packetPreviewAreaBox.set_homogeneous(False)
 
-		box.pack_start(labelBox, True, False, 0)
+		box.pack_start(labelBox, True, False, 5)
 		box.pack_start(buttonBox, True, False, 5)
 		box.pack_start(projectDissectorBox, True, True, 5)
 		box.pack_start(packetPreviewAreaBox, True, True, 5)
@@ -68,64 +68,64 @@ class MainWindow(Gtk.Window):
 		#Save Project Button
 		SaveProjectButton = Gtk.Button(label = "Save Project")
 		SaveProjectButton.connect("clicked", self.SaveProjectButton_clicked)
-		buttonBox.pack_start(SaveProjectButton, True, True, 0)
+		buttonBox.pack_start(SaveProjectButton, True, True, 5)
 
 		#Close Project Button
 		CloseProjectButton = Gtk.Button(label = "Close Project")
 		CloseProjectButton.connect("clicked", self.CloseProjectButton_clicked)
-		buttonBox.pack_start(CloseProjectButton, True, True, 0)
+		buttonBox.pack_start(CloseProjectButton, True, True, 5)
 
 		#Switch Workspace Button
 		SwitchWorkspaceButton = Gtk.Button(label = "Switch Workspace")
 		SwitchWorkspaceButton.connect("clicked", self.SwitchWorkspaceButton_clicked)
-		buttonBox.pack_start(SwitchWorkspaceButton, True, True, 0)
+		buttonBox.pack_start(SwitchWorkspaceButton, True, True, 5)
 
 		#Import Project Button
 		ImportProjectButton = Gtk.Button(label = "Import Project")
 		ImportProjectButton.connect("clicked", self.ImportProjectButton_clicked)
-		buttonBox.pack_start(ImportProjectButton, True, True, 0)
+		buttonBox.pack_start(ImportProjectButton, True, True, 5)
 
 		#Export Project Button
 		ExportProjectButton = Gtk.Button(label = "Export Project")
 		ExportProjectButton.connect("clicked", self.ExportProjectButton_clicked)
-		buttonBox.pack_start(ExportProjectButton, True, True, 0)
+		buttonBox.pack_start(ExportProjectButton, True, True, 5)
 
 		#Generate Dissector Script Button
 		GenerateDissectorScriptButton = Gtk.Button(label="Generate Dissector Script")
 		GenerateDissectorScriptButton.connect("clicked", self.GenerateDissectorScriptButton_clicked)
-		buttonBox.pack_start(GenerateDissectorScriptButton, True, True, 0)
+		buttonBox.pack_start(GenerateDissectorScriptButton, True, True, 5)
 
 		#Organize Views Button
 		OrganizeViewsButton = Gtk.Button(label="Organize Views")
 		OrganizeViewsButton.connect("clicked", self.OrganizeViewsButton_clicked)
-		buttonBox.pack_start(OrganizeViewsButton, True, True, 0)
+		buttonBox.pack_start(OrganizeViewsButton, True, True, 5)
 
 		#Open PCAP Button
 		OpenPCAPButton = Gtk.Button(label="Open PCAP")
 		OpenPCAPButton.connect("clicked", self.OpenPCAPButton_clicked)
-		buttonBox.pack_start(OpenPCAPButton, True, True, 0)
+		buttonBox.pack_start(OpenPCAPButton, True, True, 5)
 
 
 		#************************************************Project Navigator Area**************************************************
 		store = Gtk.TreeStore(str)
 
-		row1 = store.append(None, ['Project1        '])
+		row1 = store.append(None, ['ProjectA'])
+		store.append(row1, ['dissectorA1'])
 
-		store.append(row1, ['dissector1'])
-		store.append(row1, ['pcap1'])
+		row2 = store.append(None, ['Project B'])
+		store.append(row2, ['dissectorB1'])
 
-		row2 = store.append(None, ['Project 2       '])
-		store.append(row2, ['dissector2'])
-		store.append(row2, ['pcap2'])
+		row2 = store.append(None, ['Project C'])
+		store.append(row2, ['dissectorC1'])
 
 		treeView = Gtk.TreeView(store)
-		treeViewColumn = Gtk.TreeViewColumn('Project')
+		treeViewColumn = Gtk.TreeViewColumn('Project Navigator')
 		treeView.append_column(treeViewColumn)
 
 		cell = Gtk.CellRendererText()
 		treeViewColumn.pack_start(cell, True)
 		treeViewColumn.add_attribute(cell, 'text', 0)
-		projectNavigatorBox.add(treeView)
+		projectNavigatorBox.pack_start(treeView, True, True, 5)
 
 
 
@@ -139,29 +139,29 @@ class MainWindow(Gtk.Window):
 		#*************************************************Packet Preview Area***************************************************
 		
 		previewAreas = Gtk.Notebook()
-		packetPreviewAreaBox.add(previewAreas)
+		packetPreviewAreaBox.pack_start(previewAreas, True, True, 5)
 
 		packetStreamBox = Gtk.Box(spacing = 5)
 		packetStreamBox.set_homogeneous(False)
-		packetStreamBox.set_border_width(10)
+		packetStreamBox.set_border_width(5)
 		packetStreamBox.add(Gtk.Label('Test'))
 		previewAreas.append_page(packetStreamBox, Gtk.Label('Packet Stream Area View'))
 
 		dissectedStreamBox = Gtk.Box(spacing = 5)
 		dissectedStreamBox.set_homogeneous(False)
-		dissectedStreamBox.set_border_width(10)
+		dissectedStreamBox.set_border_width(5)
 		dissectedStreamBox.add(Gtk.Label('Test'))
 		previewAreas.append_page(dissectedStreamBox, Gtk.Label('Dissected Stream Area View'))
 
 		rawDataBox = Gtk.Box(spacing = 5)
 		rawDataBox.set_homogeneous(False)
-		rawDataBox.set_border_width(10)
+		rawDataBox.set_border_width(5)
 		rawDataBox.add(Gtk.Label('Test'))
 		previewAreas.append_page(rawDataBox, Gtk.Label('Raw Data Area'))
 
 		consoleBox = Gtk.Box(spacing = 5)
 		consoleBox.set_homogeneous(False)
-		consoleBox.set_border_width(10)
+		consoleBox.set_border_width(5)
 		consoleBox.add(Gtk.Label('Test'))
 		previewAreas.append_page(consoleBox, Gtk.Label('Console Area'))
 
