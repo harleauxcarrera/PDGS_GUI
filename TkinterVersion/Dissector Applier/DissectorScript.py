bb2348b192bbcd2d9784e1ad972c83fecf374e89
hliuUTEP <heyi1717@gmail.com>
2018-05-08 16:42:08 -0600

    Dissector Applier Subsystem

---
+++ TkinterVersion/Dissector Applier/DissectorScript.py
@@ -0,0 +1,14 @@
+#import ProtocolDecisionTree
+
+class DissectorScript:
+
+    def generateDissectorScript(self, pdt):
+        print"hi"
+
+    def getDissectorScript():
+
+
+#pdt = ProtocolDecisionTree.getPDT()
+pdt = 1;
+DS = DissectorScript()
+DS.generateDissectorScript(pdt)
