2f2326992461a4ab4259bfafae65cff8bb7cd433
Alex5Bal <abalderrama5@outlook.com>
2018-05-14 17:52:39 -0600

    Packet Stream Area

---
+++ TkinterVersion/packetPreviewArea.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+
+class packetPreviewArea(Frame):
+
+    def __init__(self):
+
+        Frame.__init__(self, witdth=180, height=160)
+        self.title("Packet Preview Area")
